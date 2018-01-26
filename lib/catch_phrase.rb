05669436f793defdd40f900ad29a8ca728476f33
Harleigh Abel <harleighabel@gmail.com>
2018-01-26 20:00:48 +0000

    automatically backed up by learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,3 +1,3 @@
 def catch_phrase(saying)
   puts saying
-end  
+end
--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,6 +1,5 @@
+def rescue_princess_peach(message)
+  status = "needs rescue"
+  message
+end  
 
-
-## Comment the below back in to see method scope in action!
-
-##rescue_princess_peach
-##puts status
